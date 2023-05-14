dc9e282fc25ad8efd16af7f475dd572a9f540ca6
仲川舞 <barairopop0319@gmail.com>
2023-05-14 20:29:09 +0900

    Revert "add: Added lesson28 with new directory structure."
    This reverts commit c6e1622aba62babd900836ae8a3c1ca15ea44d59.

--- README.md
+++
@@ -1,3 +0,0 @@
-# front-end-lesson
-This repository is a JavaScript challenge to becoming a front-end engineer.
-Created by Mr.Morita.
--- src/lesson28/js/utils/createAttributedElements.js
+++
@@ -1,8 +0,0 @@
-export const createAttributedElements = ({tag,valuesByAttributes,str=""}) =>{
-    const element = document.createElement(tag);
-    Object.keys(valuesByAttributes).forEach((attribute) => {
-        element.setAttribute(attribute, valuesByAttributes[attribute]);
-    });
-    element.textContent = str;
-    return element;
-}
